e8791be4122ac2bbe67c1824f228d56a3c5cf614
fm-labs <flowmotionlabs@gmail.com>
2020-03-03 18:38:37 +0100

    added fixtures

---
+++ tests/Fixture/GroupsUsersFixture.php
@@ -0,0 +1,26 @@
+<?php
+namespace User\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * GroupsUsersFixture
+ *
+ */
+class GroupsUsersFixture extends TestFixture
+{
+
+    /**
+     * Import
+     *
+     * @var array
+     */
+    public $import = ['table' => 'user_groups_users'];
+
+    /**
+     * Records
+     *
+     * @var array
+     */
+    public $records = [];
+}
